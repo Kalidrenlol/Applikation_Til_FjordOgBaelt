c450ecf4ddeaa5201d78c1ff1283753ac897d680
Thor <Thorlind89@hotmail.com>
2018-03-11 11:57:19 +0100

    Language for android

--- Assets/Scripts/Language/LocalizationManager.cs
+++ Assets/Scripts/Language/LocalizationManager.cs
@@ -1,4 +1,4 @@
-﻿﻿using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System.IO;
@@ -10,6 +10,8 @@ public class LocalizationManager : MonoBehaviour {
 	private Dictionary<string, string> localizedText;
 	private bool isReady = false;
 	private string missingTextString = "Localized text not found";
+	private string filePath;
+	private string dataAsJson;
 
 	void Awake () {
 		if (instance == null) {
@@ -22,15 +24,39 @@ public class LocalizationManager : MonoBehaviour {
 
 	public void LoadLocalizedText(string fileName) {
 		localizedText = new Dictionary<string, string> ();
-		string filePath = Path.Combine (Application.streamingAssetsPath, fileName);
+		filePath = Path.Combine(Application.streamingAssetsPath + "/", fileName);
+		#if UNITY_EDITOR
+			Debug.Log("Unity Editor");
+			
+			dataAsJson= File.ReadAllText (filePath);
+		#endif
+
+		#if UNITY_IPHONE
+			Debug.Log("Iphone");
+		#endif
+
+		#if UNITY_IOS
+			Debug.Log("IOS");
+		#endif
+
+		#if UNITY_ANDROID
+			Debug.Log("Android");
+			WWW reader = new WWW (filePath);
+			while (!reader.isDone) {}
+			dataAsJson = reader.text;
+		#endif
+
+		Debug.Log (filePath);
+
+		LocalizationData loadedData = JsonUtility.FromJson<LocalizationData> (dataAsJson);
+
+		for (int i = 0; i < loadedData.items.Length; i++) {
+			localizedText.Add (loadedData.items [i].key, loadedData.items [i].value);   
+		}
 
 		if (File.Exists (filePath)) {
-			string dataAsJson = File.ReadAllText (filePath);
-			LocalizationData loadedData = JsonUtility.FromJson<LocalizationData> (dataAsJson);
+			 
 
-			for (int i = 0; i < loadedData.items.Length; i++) {
-				localizedText.Add (loadedData.items [i].key, loadedData.items [i].value);   
-			}
 
 			Debug.Log ("Data loaded, dictionary contains: " + localizedText.Count + " entries");
 		} else {
