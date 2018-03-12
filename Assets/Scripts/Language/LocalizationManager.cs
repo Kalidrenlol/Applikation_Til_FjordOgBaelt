5268facd65bb898870bc5d828cfbcc3d8b8d7eb9
Thor <Thorlind89@hotmail.com>
2018-03-12 23:34:40 +0100

    CLeanup ios

--- Assets/Scripts/Language/LocalizationManager.cs
+++ Assets/Scripts/Language/LocalizationManager.cs
@@ -28,66 +28,32 @@ public class LocalizationManager : MonoBehaviour {
 	public void LoadLocalizedText(string fileName) {
 		localizedText = new Dictionary<string, string> ();
 		filePath = Path.Combine(Application.streamingAssetsPath + "/", fileName);
+
 		#if UNITY_EDITOR
-			Debug.Log("Unity Editor");
-			
 			dataAsJson= File.ReadAllText (filePath);
 		#endif
 
-		/*#if UNITY_IPHONE
-			Debug.Log("Iphone");
-			filePath =  Application.dataPath + "/Raw/" + filename;
-		Debug.Log("Android");
-		WWW reader = new WWW (filePath);
-		while (!reader.isDone) {}
-		dataAsJson = reader.text;
-		#endif*/
-
 		#if UNITY_IOS
-			Debug.Log("IOS");
-            filePath = Application.dataPath + "/Raw/" + fileName;
             jsonTest = JsonMapper.ToObject(File.ReadAllText(Application.dataPath + "/Raw/" + fileName));
-        for (int i = 0; i < jsonTest[0].Count; i++) {
-            print("KIG HER: --- " + jsonTest[0][i]["key"]);
-            print("KIG HER: --- " + jsonTest[0][i]["value"]);
-        }
-            
-            Debug.Log("Fil fundet: " + filePath);
-			WWW reader = new WWW (filePath);
-			while (!reader.isDone) {}
-        print("1");
-			dataAsJson = reader.text;
-        print(reader.text);
-        print("2");
+			
+			for (int i = 0; i < jsonTest[0].Count; i++) {
+				localizedText.Add (jsonTest[0][i]["key"].ToString(), jsonTest[0][i]["value"].ToString());   
+			}
 		#endif
 
 		#if UNITY_ANDROID
-			Debug.Log("Android");
 			WWW reader = new WWW (filePath);
 			while (!reader.isDone) {}
 			dataAsJson = reader.text;
 		#endif
 
-		Debug.Log (filePath);
-        print("3");
+		#if UNITY_EDITOR || UNITY_ANDROID
+			LocalizationData loadedData = JsonUtility.FromJson<LocalizationData> (dataAsJson);
 
-		LocalizationData loadedData = JsonUtility.FromJson<LocalizationData> (dataAsJson);
-
-		/*for (int i = 0; i < loadedData.items.Length; i++) {
-			localizedText.Add (loadedData.items [i].key, loadedData.items [i].value);   
-		}*/
-		for (int i = 0; i < jsonTest[0].Count; i++) {
-			localizedText.Add (jsonTest[0][i]["key"].ToString(), jsonTest[0][i]["value"].ToString());   
-		}
-
-		if (File.Exists (filePath)) {
-			 
-
-
-			Debug.Log ("Data loaded, dictionary contains: " + localizedText.Count + " entries");
-		} else {
-			Debug.LogError ("Cannot find file!");
-		}
+			for (int i = 0; i < loadedData.items.Length; i++) {
+				localizedText.Add (loadedData.items [i].key, loadedData.items [i].value);   
+			}
+		#endif
 
 		isReady = true;
 	}
